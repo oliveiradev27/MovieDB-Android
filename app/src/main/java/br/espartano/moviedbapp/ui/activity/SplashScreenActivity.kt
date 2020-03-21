0a59621c4e912d7bbdc95adc235370631ab02b7c
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2020-03-21 17:40:06 -0300

    Adiciona tela de splash

---
+++ app/src/main/java/br/espartano/moviedbapp/ui/activity/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package br.espartano.moviedbapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.espartano.moviedbapp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
