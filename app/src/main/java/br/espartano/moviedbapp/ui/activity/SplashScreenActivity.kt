8a4fe22572f5c7a67a71809eb25b8b3c1e1d09de
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2020-03-22 09:39:50 -0300

    Organiza os códigos da splash

--- app/src/main/java/br/espartano/moviedbapp/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/br/espartano/moviedbapp/ui/activity/SplashScreenActivity.kt
@@ -1,13 +1,27 @@
 package br.espartano.moviedbapp.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import br.espartano.moviedbapp.R
+import br.espartano.moviedbapp.list.ui.ListMoviesActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        initializeListMovies()
+    }
+
+
+    private fun initializeListMovies() {
+        findViewById<ImageView>(R.id.img_logo).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, ListMoviesActivity::class.java))
+            finish()
+        },
+            2000L
+        )
     }
 }
