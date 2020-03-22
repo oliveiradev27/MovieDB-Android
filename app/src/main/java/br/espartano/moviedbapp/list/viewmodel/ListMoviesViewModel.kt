ece22eb90efe8ed0130cec82568fb55ed1101bbc
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2020-03-22 09:38:39 -0300

    Cria o ViewModel e máquina de estados para tela de Listagem.

---
+++ app/src/main/java/br/espartano/moviedbapp/list/viewmodel/ListMoviesViewModel.kt
@@ -0,0 +1,33 @@
+package br.espartano.moviedbapp.list.viewmodel
+
+import androidx.lifecycle.*
+import br.espartano.moviedbapp.repository.MoviesRepository
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class ListMoviesViewModel(private val repository: MoviesRepository): ViewModel() {
+
+    private val states = MutableLiveData<ListMoviesStates>()
+
+    fun getState() : LiveData<ListMoviesStates> = states
+
+    fun getPopularMovies() {
+        viewModelScope.launch {
+            withContext(IO) {
+                try {
+                    states.postValue(ListMoviesStates.LoadSuccessMovies(repository.getPopularMovies()))
+                } catch (t: Throwable) {
+                    states.postValue(ListMoviesStates.Error(t))
+                }
+            }
+        }
+    }
+
+    class ListMoviesViewModelFactory(private val repository: MoviesRepository): ViewModelProvider.Factory {
+
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+            ListMoviesViewModel(repository) as T
+
+    }
+}
