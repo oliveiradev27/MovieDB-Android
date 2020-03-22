ea03934b0060ea8d49fd752affc79545982e7b56
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2020-03-22 09:40:43 -0300

    Adiciona telas no manifest

---
+++ app/src/main/java/br/espartano/moviedbapp/list/ui/ListMoviesActivity.kt
@@ -0,0 +1,56 @@
+package br.espartano.moviedbapp.list.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.espartano.moviedbapp.R
+import br.espartano.moviedbapp.data.Movie
+import br.espartano.moviedbapp.list.adapters.MoviesAdapter
+import br.espartano.moviedbapp.list.viewmodel.ListMoviesStates
+import br.espartano.moviedbapp.list.viewmodel.ListMoviesViewModel
+import br.espartano.moviedbapp.repository.MoviesNetworkRepository
+
+class ListMoviesActivity : AppCompatActivity() {
+
+    private val movies = mutableListOf<Movie>()
+    private val recyclerMovies : RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.recycler_movies)
+    }
+
+    private val viewModel: ListMoviesViewModel by lazy {
+        ViewModelProvider(this@ListMoviesActivity,
+            ListMoviesViewModel.ListMoviesViewModelFactory(MoviesNetworkRepository()))
+            .get(ListMoviesViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_movies)
+        configureStates()
+        initializeViews()
+    }
+
+    private fun configureStates() {
+        viewModel.getState()
+            .observe(this, Observer {
+            when (it) {
+                is ListMoviesStates.Loading -> Toast.makeText(this, "carregando..", Toast.LENGTH_LONG).show()
+                is ListMoviesStates.LoadSuccessMovies ->{
+                    movies.addAll(it.movies)
+                    recyclerMovies.adapter?.notifyDataSetChanged()
+                }
+                is ListMoviesStates.Error -> Toast.makeText(this, it.t.message, Toast.LENGTH_LONG).show()
+            }
+        })
+        viewModel.getPopularMovies()
+    }
+
+    private fun initializeViews() {
+        recyclerMovies.adapter = MoviesAdapter(movies)
+    }
+}
