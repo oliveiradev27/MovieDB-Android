f85b0f2c7f920d210380c746d6a46dec6f1b8bf2
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2020-06-07 17:49:22 -0300

    Adiciona inicialização do Koin no projeto

---
+++ app/src/main/java/br/espartano/moviedbapp/app/MyApplication.kt
@@ -0,0 +1,20 @@
+package br.espartano.moviedbapp.app
+
+import android.app.Application
+import br.espartano.moviedbapp.di.appModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MyApplication)
+            modules(appModules)
+        }
+    }
+}
