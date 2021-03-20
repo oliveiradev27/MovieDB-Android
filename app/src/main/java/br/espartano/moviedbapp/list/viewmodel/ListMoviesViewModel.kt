ef0f465c67da4124c35e898283ea09b0e290a0fb
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2021-03-20 18:40:47 -0300

    Atualiza dependências

--- app/src/main/java/br/espartano/moviedbapp/list/viewmodel/ListMoviesViewModel.kt
+++ app/src/main/java/br/espartano/moviedbapp/list/viewmodel/ListMoviesViewModel.kt
@@ -3,26 +3,21 @@ package br.espartano.moviedbapp.list.viewmodel
 import androidx.lifecycle.*
 import br.espartano.moviedbapp.repository.MoviesRepository
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class ListMoviesViewModel(private val repository: MoviesRepository): ViewModel() {
+class ListMoviesViewModel(private val repository: MoviesRepository) : ViewModel() {
 
     private val states = MutableLiveData<ListMoviesStates>()
 
-    fun getState() : LiveData<ListMoviesStates> = states
+    fun getState(): LiveData<ListMoviesStates> = states
 
     fun getPopularMovies() {
         states.postValue(ListMoviesStates.Loading)
-        viewModelScope.launch {
-            withContext(IO) {
-                try {
-                    delay(5000)
-                    states.postValue(ListMoviesStates.LoadSuccessMovies(repository.getPopularMovies()))
-                } catch (t: Throwable) {
-                    states.postValue(ListMoviesStates.Error(t))
-                }
+        viewModelScope.launch(IO) {
+            try {
+                states.postValue(ListMoviesStates.LoadSuccessMovies(repository.getPopularMovies()))
+            } catch (t: Throwable) {
+                states.postValue(ListMoviesStates.Error(t))
             }
         }
     }
